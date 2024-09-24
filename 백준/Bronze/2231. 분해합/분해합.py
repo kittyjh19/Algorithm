c2abe5523eff52ff619413397fdc00c09cce4bd1
Juhee Hong <163083158+kittyjh19@users.noreply.github.com>
2024-09-24 20:11:53 +0900

    분해합.py

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,7 +1,7 @@
-n = int(input()) 
-answer = 0
+n = int(input()) # 입력 값(n) 받기
+answer = 0 # 결과를 담을 변수를 생성(answer)
 
-for i in range(1, n+1):
+for i in range(1, n+1): # for문으로 1부터 n까지 모든 수를 계산해 분해합 구하기
     nums = list(map(int, str(i)))
     answer = sum(nums) + i
     if answer == n:
